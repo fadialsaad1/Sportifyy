951a4e43c3f5a203910d5cf2dbfcde9b780df767
mng48301 <mng48301@gmail.com>
2025-11-01 23:43:28 -0400

    test

--- sportifyy/src/main.jsx
+++ sportifyy/src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import ProfilePage from "./Profile";
 import "./index.css"; // <-- make sure Tailwind is imported
 
@@ -6,4 +7,16 @@ function App() {
     return <ProfilePage />;
 }
 
-export default App;
+// Mount the React app into the DOM (Vite loads this file as a module from index.html)
+const container = document.getElementById("root");
+if (container) {
+    const root = createRoot(container);
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+} else {
+    // eslint-disable-next-line no-console
+    console.error('Root container not found: make sure <div id="root"></div> exists in index.html');
+}
